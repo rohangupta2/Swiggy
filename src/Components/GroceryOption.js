22f0d4d2ac14afb33238ae9073ae84646b50042c
Rohan Gupta <Rohangupta3141@gmail.com>
2026-02-28 19:54:02 +0530

    day1

---
+++ src/Components/GroceryOption.js
@@ -0,0 +1,17 @@
+import {GroceryGridCards} from "../Utils/Grocery"
+import Grocerycard from "./Grocerycard"
+
+export default function GroceryOption(){
+
+
+    return(
+         <div className="mt-20 w-[80%] container mx-auto">
+            <h1>Shop Groceries on Instamart</h1>
+            <div className="w-[80%] container mx-auto flex flex-wrap mt-20 gap-3">
+                    {
+                        GroceryGridCards.map((foodData)=><Grocerycard key={foodData.id} foodData={foodData}></Grocerycard>)
+                    }
+                  </div>
+        </div>
+    )
+}
